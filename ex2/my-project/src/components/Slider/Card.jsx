3b933dad614a674712a330eccce0aeef7d9a1c67
Serhii Chyipesh <chiypesh200059@gmail.com>
2023-12-23 00:17:30 +0200

    ex2

--- ex2/my-project/src/components/Slider/Card.jsx
+++ ex2/my-project/src/components/Slider/Card.jsx
@@ -3,7 +3,7 @@ const Card = ({ id, image, title, description, userName, data, userPhoto }) => {
     <div className="mx-auto mb-12 max-w-[370px] bg-white drop-shadow-2xl">
       <img src={image} alt={title} className="h-44 w-full object-cover" />
       <div className="flex flex-col gap-y-4 px-5 py-5">
-        <h2 className="text-[18px] font-bold text-blue-900 md:text-[22px]">
+        <h2 className="text-[18px] font-bold uppercase text-blue-900 md:text-[22px]">
           {title}
         </h2>
         <p>{description}</p>
@@ -13,8 +13,8 @@ const Card = ({ id, image, title, description, userName, data, userPhoto }) => {
             alt={`photo of ${userName}`}
             className="rounded-full"
           />
-          <div className="">
-            <h3 className="text-[16px] md:text-[18px]">{userName}</h3>
+          <div>
+            <h3 className="text-[16px] uppercase md:text-[18px]">{userName}</h3>
             <p className="text-blue-300">{data}</p>
           </div>
         </div>
