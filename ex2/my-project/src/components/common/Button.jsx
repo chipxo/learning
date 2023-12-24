b0f5cb59ef1fd57c8b84a7fae110e5da02efad34
Serhii Chyipesh <chiypesh200059@gmail.com>
2023-12-24 22:24:53 +0200

    ex2

--- ex2/my-project/src/components/common/Button.jsx
+++ ex2/my-project/src/components/common/Button.jsx
@@ -3,7 +3,7 @@ const Button = ({ text, onClick, buttonText }) => {
     <div>
       <button
         onClick={onClick}
-        className={`to-light-blue from-mid-purple border px-6 py-2 font-[Montserrat] uppercase transition-all duration-200 hover:bg-gradient-to-br hover:text-white md:px-10 md:py-3 
+        className={`to-light-blue from-mid-purple border px-6 py-2 font-[Montserrat] uppercase hover:bg-gradient-to-br hover:text-white md:px-10 md:py-3 
         ${buttonText === "less" ? "bg-dark-blue text-white" : ""}`}
       >
         {text}
